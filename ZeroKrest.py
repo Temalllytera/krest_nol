78f2efe56b9b040aad9f0cc1c7cfc4ec67847fc1
Артем Булавин <bulavin@Air-Artem.Home>
2022-01-28 19:30:04 +0300

    refactoring code

--- ZeroKrest.py
+++ ZeroKrest.py
@@ -1,10 +1,10 @@
 board = list(range(1,10))
 
 def draw_board(board):
-    print ("-" * 13)
+    print("-" * 13)
     for i in range(3):
-        print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-        print ("-" * 13)
+        print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print("-" * 13)
 
 def take_input(player_token):
     valid = False
@@ -13,16 +13,16 @@ def take_input(player_token):
         try:
             player_answer = int(player_answer)
         except:
-            print ("Некорректный ввод. Вы уверены, что ввели число?")
+            print("Некорректный ввод. Вы уверены, что ввели число?")
             continue
         if player_answer >= 1 and player_answer <= 9:
             if (str(board[player_answer-1]) not in "XO"):
                 board[player_answer-1] = player_token
                 valid = True
             else:
-                print ("Эта клеточка уже занята")
+                print("Эта клеточка уже занята")
         else:
-            print ("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+            print("Некорректный ввод. Введите число от 1 до 9 чтобы сделать ход.")
 
 def check_win(board):
     win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
@@ -32,6 +32,7 @@ def check_win(board):
     return False
 
 def main(board):
+    print('Игра начинается')
     counter = 0
     win = False
     while not win:
@@ -44,11 +45,11 @@ def main(board):
         if counter > 4:
             tmp = check_win(board)
             if tmp:
-                print (tmp, "выиграл!")
+                print(tmp, "выиграл!")
                 win = True
                 break
         if counter == 9:
-            print ("Ничья!")
+            print("Ничья!")
             break
     draw_board(board)
 
